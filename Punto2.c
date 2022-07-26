fbc3823bf3a46dc3185da0a8affebfdec2c5a3f8
NazaretCS <nazaretcampos27@gmail.com>
2022-07-26 11:33:26 -0300

    commit por las dudas

--- Punto2.c
+++ Punto2.c
@@ -12,15 +12,7 @@ typedef struct Tarea {
 
 int SolicitarCantidadTareas();
 void CargarTareas( Tarea **ArregloTareas, int CantTareas);
-<<<<<<< HEAD
 void BuscaTareaPalabra(Tarea **ArregloTareas, Tarea **ArregloTarasRealizadas, int CantTareas, int realizadas, char *busqueda);
-=======
-void BuscarTarea(Tarea **ArregloTareas, Tarea **ArregloTareasRealizadas, int CantTareas, int realizadas);
-void MostrarTarea(Tarea Tarea);
-int ConsultarTareas(Tarea **ArregloTareas, Tarea **ArregloTareasRealizadas, int CantTareas);
-void MostrarTareasPendientesYRealizadas(Tarea **ArregloTareas, Tarea **ArregloTareasRealizadas, int CantTareas, int realizadas);
-
->>>>>>> BuscaTarea
 
 int main(void){
     int CantTareas, realizadas;
@@ -44,6 +36,7 @@ int main(void){
     
     realizadas = ConsultarTareas(ArregloTareas, ArregloTareasRealizadas, CantTareas);
 
+    BuscaTareaPalabra(ArregloTareas, ArregloTareasRealizadas, CantTareas, realizadas, "Contar");
 
 
     return 0;
@@ -137,16 +130,15 @@ void MostrarTareasPendientesYRealizadas(Tarea **ArregloTareas, Tarea **ArregloTa
     }
 
     printf("\n\nTareas Incompletas\n");
-    for (int j = 0; j < CantTareas; j++)
+    for (int i = 0; i < CantTareas; i++)
     {
-        if (ArregloTareas[j] != NULL)
+        if (ArregloTareas[i] != NULL)
         {
-            MostrarTarea(*ArregloTareas[j]);
+            MostrarTarea(*ArregloTareas[i]);
         }        
     }
 }
 
-<<<<<<< HEAD
 
 void BuscaTareaPalabra(Tarea **ArregloTareas, Tarea **ArregloTarasRealizadas, int CantTareas, int realizadas, char *busqueda)
 {
@@ -169,42 +161,3 @@ void BuscaTareaPalabra(Tarea **ArregloTareas, Tarea **ArregloTarasRealizadas, in
         }
     }
 }
-=======
-void BuscarTareaID(Tarea **ArregloTareas, Tarea **ArregloTareasRealizadas, int CantTareas, int realizadas){
-
-    int buscar=-1;
-
-    do
-    {
-        printf("\nIngrese el ID de la Tarea que desea uvicar: \n");
-        scanf("%d",&buscar);
-        if (buscar<=0 && buscar>CantTareas)
-        {
-            printf("El valor ingresado no se encuentra en el rango de tareas existentes... Por favor intente nuevamente\n");
-        }
-        
-    } while (buscar<=0 && buscar>CantTareas);   
-    
-
-    for (int i = 0; i < realizadas; i++)
-    {        
-        if (ArregloTareasRealizadas[i]->TareaID == buscar)
-        {
-            printf("\nTarea Localizada:\n\n");
-            MostrarTarea(*ArregloTareasRealizadas[i]);
-            printf("\nEstado de la Tarea: Realizada\n");
-        }
-    }
-
-    for (int j = 0; j < CantTareas; j++)
-    {        
-        if (ArregloTareas[j]->TareaID == buscar && ArregloTareas[j] != NULL) 
-        {
-            printf("\nTarea Localizada:\n\n");
-            MostrarTarea(*ArregloTareas[j]);
-            printf("\nEstado de la Tarea: No Realizada\n");
-        }
-    }
-}
-
->>>>>>> BuscaTarea
